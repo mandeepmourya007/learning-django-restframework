a6f275d71931593d62b30410e5f357914ce8ce56
mandeep <mandeepmourya007@gmail.com>
2020-07-20 20:42:15 +0530

    view added

--- learn_rfw/learn_rfw/quickstart/views.py
+++ learn_rfw/learn_rfw/quickstart/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.models import User,Group
+from rest_framework import viewsets
+from learn_rfw.quickstart.serializers import UserSerializer,GroupSerializer
+
+'''  NOTE  queryset and serilizer_class is required '''
+#view for User serializer
+class UserViewSet(viewsets.ModelViewSet):
+
+    #API to see all users
+    queryset = User.objects.all()
+    
+    #  we need to tell which serializer to be used
+    serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+
+    #to see all groups
+    queryset = Group.objects.all()
+
+    #serializer to be used
+    serializer_class = GroupSerializer
