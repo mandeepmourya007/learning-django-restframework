fd7c8c45f7e39625c85fdbfdd6784cb18692e5c4
mandeep <mandeepmourya007@gmail.com>
2020-07-26 17:39:29 +0530

    added postgres db

--- learn_rfw/snippets/admin.py
+++ learn_rfw/snippets/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import snippets
 # Register your models here.
+
+admin.site.register(snippets)
--- learn_rfw/snippets/models.py
+++ learn_rfw/snippets/models.py
@@ -38,3 +38,17 @@ class snippets(models.Model):
     class Meta:
         ordering = ["created"]
 
+# from django.contrib.gis.db import models
+# from django.contrib.postgres.operations import CreateExtension
+# from django.db import migrations
+
+# class Migration(migrations.Migration):
+
+#     operations = [
+#         CreateExtension('postgis'),
+#     ]
+
+
+# class model1(models.Model):
+#      geom = models.GeometryField(srid=4326,blank=True,null=True)
+#      name = models.TextField(null=True)
